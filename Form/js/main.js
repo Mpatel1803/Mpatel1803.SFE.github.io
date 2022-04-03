51bd220f2a979f35223d1afa91d99fb07334e291
Manav Patel <patelmanav1803@gmail.com>
2022-04-03 22:13:29 +0300

    Initial Commit

---
+++ Form/js/main.js
@@ -0,0 +1,125 @@
+
+
+(function($) {
+
+
+    var form = $("#signup-form");
+    form.validate({
+        errorPlacement: function errorPlacement(error, element) {
+            element.before(error);
+        },
+        rules: {
+            email: {
+                email: true
+            }
+        },
+        onfocusout: function(element) {
+            $(element).valid();
+        },
+    });
+    form.children("div").steps({
+        headerTag: "h3",
+        bodyTag: "fieldset",
+        transitionEffect: "fade",
+        stepsOrientation: "vertical",
+        titleTemplate: '<div class="title"><span class="step-number">#index#</span><span class="step-text">#title#</span></div>',
+        labels: {
+            previous: 'Previous',
+            next: 'Next',
+            finish: 'Finish',
+            current: ''
+        },
+        onStepChanging: function(event, currentIndex, newIndex) {
+            if (currentIndex === 0) {
+                form.parent().parent().parent().append('<div class="footer footer-' + currentIndex + '"></div>');
+            }
+            if (currentIndex === 1) {
+                form.parent().parent().parent().find('.footer').removeClass('footer-0').addClass('footer-' + currentIndex + '');
+            }
+            if (currentIndex === 2) {
+                form.parent().parent().parent().find('.footer').removeClass('footer-1').addClass('footer-' + currentIndex + '');
+            }
+            if (currentIndex === 3) {
+                form.parent().parent().parent().find('.footer').removeClass('footer-2').addClass('footer-' + currentIndex + '');
+            }
+            if (currentIndex === 4) {
+                form.parent().parent().parent().find('.footer').removeClass('footer-3').addClass('footer-' + currentIndex + '');
+            }
+            if (currentIndex === 5) {
+                form.parent().parent().parent().find('.footer').removeClass('footer-4').addClass('footer-' + currentIndex + '');
+            }
+            if (currentIndex === 6) {
+                form.parent().parent().parent().find('.footer').removeClass('footer-5').addClass('footer-' + currentIndex + '');
+            }
+            if (currentIndex === 7) {
+                form.parent().parent().parent().find('.footer').removeClass('footer-6').addClass('footer-' + currentIndex + '');
+            }
+            if (currentIndex === 8) {
+                form.parent().parent().parent().find('.footer').removeClass('footer-7').addClass('footer-' + currentIndex + '');
+            }
+            
+            form.validate().settings.ignore = ":disabled,:hidden";
+            return form.valid();
+        },
+        onFinishing: function(event, currentIndex) {
+            form.validate().settings.ignore = ":disabled";
+            return form.valid();
+        },
+        onFinished: function(event, currentIndex) {
+            location.href = "/Form/submission.html";
+        },
+        onStepChanged: function(event, currentIndex, priorIndex) {
+
+            return true;
+        }
+    });
+
+    jQuery.extend(jQuery.validator.messages, {
+        required: "",
+        remote: "",
+        email: "",
+        url: "",
+        date: "",
+        dateISO: "",
+        number: "",
+        digits: "",
+        creditcard: "",
+        equalTo: ""
+    });
+
+    
+    var marginSlider = document.getElementById('slider-margin');
+    if (marginSlider != undefined) {
+        noUiSlider.create(marginSlider, {
+              start: [1100],
+              step: 100,
+              connect: [true, false],
+              tooltips: [true],
+              range: {
+                  'min': 100,
+                  'max': 2000
+              },
+              pips: {
+                    mode: 'values',
+                    values: [100, 2000],
+                    density: 8
+                    },
+                format: wNumb({
+                    decimals: 0,
+                    thousand: '',
+                    prefix: '$ ',
+                })
+        });
+        var marginMin = document.getElementById('value-lower'),
+	    marginMax = document.getElementById('value-upper');
+
+        marginSlider.noUiSlider.on('update', function ( values, handle ) {
+            if ( handle ) {
+                marginMax.innerHTML = values[handle];
+            } else {
+                marginMin.innerHTML = values[handle];
+            }
+        });
+    }
+})(jQuery);
+
